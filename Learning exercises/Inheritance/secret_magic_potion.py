5e2c8031a2eeddb559133e980b78a6e997ba91c6
Melvin Cornebize <107157412+melvincornebize@users.noreply.github.com>
2025-08-01 22:58:51 +0200

    last inheritance exercise of the day - password check in the infant class to access parent class

---
+++ Learning exercises/Inheritance/secret_magic_potion.py
@@ -0,0 +1,35 @@
+# Write your solution here:
+class MagicPotion:
+    def __init__(self, name: str):
+        self._name = name
+        self._ingredients = []
+
+    def add_ingredient(self, ingredient: str, amount: float):
+        self._ingredients.append((ingredient, amount))
+
+    def print_recipe(self):
+        print(self._name + ":")
+        for ingredient in self._ingredients:
+            print(f"{ingredient[0]} {ingredient[1]} grams")
+
+class SecretMagicPotion(MagicPotion):
+    def __init__(self,name,password:str):
+        super().__init__(name)
+        self._password=password
+    def add_ingredient(self, ingredient: str, amount: float, password: str):
+        if password !=self._password:
+            raise ValueError
+        super().add_ingredient(ingredient,amount)
+
+    def print_recipe(self, password: str):
+        if password !=self._password:
+            raise ValueError("wrong password")
+        
+        print(self._name + ":")
+        for ingredient in self._ingredients:
+            print(f"{ingredient[0]} {ingredient[1]} grams")
+
+
+
+
+
